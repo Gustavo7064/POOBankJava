a75dcc35c0b5a77f3cbfd3d18233296cf450441a
Gustavo Goes <gustavorochamacie12007@gmail.com>
2026-03-06 12:06:48 -0300

    Alterando erros de Sintaxe, e logico.

--- bank/src/contapoupanca.java
+++ bank/src/contapoupanca.java
@@ -1,7 +1,7 @@
-public class ContaPoupanca extends Conta {
+public class contapoupanca extends conta {
   private double rendimento;
 
-  public ContaPoupanca(int numero, Cliente cliente, double rendimento) {
+  public contapoupanca(int numero, cliente cliente, double rendimento) {
     super(numero, cliente);
     this.rendimento = rendimento;
   }
