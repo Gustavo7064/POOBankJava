681906de91681a666c428dfe46f6fd3d254ef9a2
Gustavo Goes <gustavorochamacie12007@gmail.com>
2026-03-06 00:40:29 -0300

    implementando construtor da ContaPoupanca com taxa de rendimento

--- bank/contapoupanca.java
+++ bank/contapoupanca.java
@@ -1,3 +1,9 @@
 public class ContaPoupanca extends Conta {
   private double rendimento;
+
+  public ContaPoupanca(int numero, Cliente cliente, double rendimento) {
+    super(numero, cliente);
+    this.rendimento = rendimento;
+  }
+
 }
