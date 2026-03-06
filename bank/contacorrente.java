2abc325457e8c53c8d9a531d8b0c05dabfec05f6
Gustavo Goes <gustavorochamacie12007@gmail.com>
2026-03-06 00:27:13 -0300

    adicionando atributo limite na ContaCorrente

--- bank/contacorrente.java
+++ bank/contacorrente.java
@@ -1,3 +1,7 @@
 public class ContaCorrente extends Conta {
+private double limite;
 
+ public ContaCorrente(int numero, Cliente cliente, double limite){
+        super(numero, cliente);
+        this.limite = limite;
 }
