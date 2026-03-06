41fdf48d45c866604a2d5bcdb60d586859e1a127
Gustavo Goes <gustavorochamacie12007@gmail.com>
2026-03-06 00:32:00 -0300

    Sobrrescevendo metodo sacar para considerar o limite da conta corrente

--- bank/contacorrente.java
+++ bank/contacorrente.java
@@ -1,7 +1,21 @@
 public class ContaCorrente extends Conta {
-private double limite;
 
- public ContaCorrente(int numero, Cliente cliente, double limite){
+    private double limite;
+
+    public ContaCorrente(int numero, Cliente cliente, double limite){
         super(numero, cliente);
         this.limite = limite;
+    }
+
+    @Override
+    public void sacar(double valor){
+
+        if(valor > saldo + limite){
+            System.out.println("Limite insuficiente");
+            return;
+        }
+
+        saldo -= valor;
+        historico.add(new Transacao("Saque", valor));
+    }
 }
