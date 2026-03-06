b2526280a074cc90236c38a9e57dd4567c01b279
Gustavo Goes <gustavorochamacie12007@gmail.com>
2026-03-06 00:07:03 -0300

    Implementando historico de transacoes utilizando ArrayList

---
+++ bank/src/conta.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class conta {
+
+  protected int numero;
+  protected cliente cliente;
+  protected double saldo;
+
+  protected ArrayList<transacao> historico = new ArrayList<>();
+}
