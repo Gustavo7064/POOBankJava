e2a473927a3f7e040becacb4180ea9c50b4d7a68
Gustavo Goes <gustavorochamacie12007@gmail.com>
2026-03-06 12:29:10 -0300

    configurando a main.

--- bank/src/App.java
+++ bank/src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        banco banco = new banco("Banco Digital");
+        // vou terminar de criar o menu)
+        Menu.iniciar(banco);
+
     }
 }
